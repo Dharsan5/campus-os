005fbbe2dabd4d2866d9009e188c031c265152ec
DHARSHAN SP <dharsandad@gmail.com>
2025-07-12 00:19:08 +0530

    second

--- os/src/App.jsx
+++ os/src/App.jsx
@@ -3,9 +3,15 @@ import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <>
+      <h1 className="text-4xl font-bold text-blue-600 text-center">
+        Tailwind is working! 🎉
+      </h1>
+      <h1 className="text-4xl text-center text-green-500 font-bold">
+        TailwindCSS is working! 🚀
+      </h1>
+      
+    </>
   );
 }
 
